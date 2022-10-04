1b5de4f6e8220d6d4099f1a596aada357051082b
Sandeep Ketkar <114158871+sandikets@users.noreply.github.com>
2022-10-04 16:26:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import URlError
+from urllib.error import URLError
 streamlit.header('Breakfast Menu')
 streamlit.text(':) Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
