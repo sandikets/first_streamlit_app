d38cbe4f3dfdfac2404a2e94f2ca3756e6e7203c
Sandeep Ketkar <114158871+sandikets@users.noreply.github.com>
2022-10-04 15:30:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 
 import streamlit
 import pandas
+import snowflake.connector
 
 streamlit.header('Breakfast Menu')
 streamlit.text(':) Omega 3 & Blueberry Oatmeal')
@@ -36,7 +37,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
